2207d356829a66e90ae899307f842a1653aa5ccb
khasan0330 <93327758+khasan0330@users.noreply.github.com>
2022-12-14 19:52:18 +0500

    Первое отображения странички, локальный маршрутизатор

--- django_1/cooking/views.py
+++ django_1/cooking/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Category, Post
 
-# Create your views here.
+def index(request):
+    posts = Post.objects.all()
+
+    context = {
+        'title': 'Главная страница',
+        'posts': posts
+    }
+
+    return render(request, 'cooking/index.html', context)
+
+
