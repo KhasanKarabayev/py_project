706f8a8d827d3eb028b33f952c5d08f26c9cc205
khasan0330 <93327758+khasan0330@users.noreply.github.com>
2022-12-14 19:37:51 +0500

    Марофет админки

--- django_1/cooking/admin.py
+++ django_1/cooking/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category, Post
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'watched', 'is_published', 'category', 'created_at', 'updated_at']
+    list_display_links = ['id', 'title']
+    list_editable = ['is_published']
+    readonly_fields = ['watched']
+    list_filter = ['title', 'category', 'watched']
+
+
+admin.site.register(Category)
+admin.site.register(Post, PostAdmin)
--- django_1/cooking/models.py
+++ django_1/cooking/models.py
@@ -2,15 +2,29 @@ from django.db import models
 
 
 class Category(models.Model):
-    title = models.CharField(max_length=150)
+    title = models.CharField(max_length=150, verbose_name='Название категории')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.TextField(default='Скоро все будет...')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    photo = models.ImageField(upload_to='photos/', blank=True, null=True)
-    watched = models.IntegerField(default=0)
-    is_published = models.BooleanField(default=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255, verbose_name='Заголовок статьи')
+    content = models.TextField(default='Скоро все будет...', verbose_name='Текст статьи')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
+    photo = models.ImageField(upload_to='photos/', blank=True, null=True, verbose_name='Изображения')
+    watched = models.IntegerField(default=0, verbose_name='Просмотры')
+    is_published = models.BooleanField(default=True, verbose_name='Публикация')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
