6ef378914b7f1d0e223b0c7fc960b3e7b509b3bf
khasan0330 <93327758+khasan0330@users.noreply.github.com>
2022-12-15 15:11:55 +0500

    Переход по категориям + продолжение распила страниц

--- django_1/cooking/views.py
+++ django_1/cooking/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from .models import Category, Post
 
+
 def index(request):
     posts = Post.objects.all()
     categories = Category.objects.all()
-
 
     context = {
         'title': 'Главная страница',
@@ -15,3 +15,14 @@ def index(request):
     return render(request, 'cooking/index.html', context)
 
 
+def category_list(request, pk):
+    posts = Post.objects.filter(category_id=pk)
+    categories = Category.objects.all()
+
+    context = {
+        'title': posts[0].category.title if posts else 'Нет статей данной категории',
+        'posts': posts,
+        'categories': categories
+    }
+
+    return render(request, 'cooking/index.html', context)
