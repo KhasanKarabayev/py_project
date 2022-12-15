3a3fc1cb5d697d9f04f8171f230b078f8ce53d31
khasan0330 <93327758+khasan0330@users.noreply.github.com>
2022-12-15 15:27:32 +0500

    Убираем повторяющиеся элементы с вьюхи, по соглашению концепции DRY

--- django_1/cooking/views.py
+++ django_1/cooking/views.py
@@ -4,12 +4,12 @@ from .models import Category, Post
 
 def index(request):
     posts = Post.objects.all()
-    categories = Category.objects.all()
+
 
     context = {
         'title': 'Главная страница',
         'posts': posts,
-        'categories': categories
+
     }
 
     return render(request, 'cooking/index.html', context)
@@ -17,12 +17,11 @@ def index(request):
 
 def category_list(request, pk):
     posts = Post.objects.filter(category_id=pk)
-    categories = Category.objects.all()
 
     context = {
         'title': posts[0].category.title if posts else 'Нет статей данной категории',
         'posts': posts,
-        'categories': categories
+
     }
 
     return render(request, 'cooking/index.html', context)
