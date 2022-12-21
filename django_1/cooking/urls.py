0083acce6a1cad2d3c54985911505b4066c82013
khasan0330 <93327758+khasan0330@users.noreply.github.com>
2022-12-21 16:33:53 +0500

    авторизацию через JWT-токенам по документации

--- django_1/cooking/urls.py
+++ django_1/cooking/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import *
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 urlpatterns = [
     path('', index, name='index'),
@@ -8,6 +9,11 @@ urlpatterns = [
     path('posts/api/', CookingAPI.as_view(), name='CookingAPI'),
     path('posts/api/<int:pk>', CookingAPIDetail.as_view(), name='CookingAPIDetail'),
     path('categories/api/', CookingCategoryAPI.as_view(), name='CookingCategoryAPI'),
-    path('categories/api/<int:pk>', CookingCategoryAPIDetail.as_view(), name='CookingCategoryAPIDetail')
+    path('categories/api/<int:pk>', CookingCategoryAPIDetail.as_view(), name='CookingCategoryAPIDetail'),
+
+    # Doc: https://django-rest-framework-simplejwt.readthedocs.io/en/latest/getting_started.html#installation
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 
 ]
