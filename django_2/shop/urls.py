0e9fbb3d8139a470146bc8667c8895c8078595c3
khasan0330 <93327758+khasan0330@users.noreply.github.com>
2023-01-06 12:14:45 +0500

    index

--- django_2/shop/urls.py
+++ django_2/shop/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('', ProductList.as_view(), name='product_list'),
 
 ]
--- django_2/shop/views.py
+++ django_2/shop/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView
+from .models import Category, Product
 
-# Create your views here.
+
+class ProductList(ListView):
+    model = Product
+    extra_context = {
+        'title': 'Главная страница'
+    }
+    template_name = 'shop/product_list.html'
