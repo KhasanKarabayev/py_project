9f14bd060694f877be001aede1c76c736a5b6ad5
khasan0330 <93327758+khasan0330@users.noreply.github.com>
2023-01-09 09:59:38 +0500

    Работа с корзинкой заказов, Часть 5

--- django_2/shop/urls.py
+++ django_2/shop/urls.py
@@ -14,5 +14,8 @@ urlpatterns = [
     path('my_favorite/', FavoriteProductsView.as_view(), name='favorite_products'),
     path('save_mail/', save_email, name='save_mail'),
     path('send_mail/', send_mail_to_customers, name='send_mail'),
+    path('cart/', cart, name='cart'),
+    path('to_cart/<int:product_id>/<str:action>/', to_cart, name='to_cart'),
+    path('checkout/', checkout, name='checkout'),
 
 ]
