4908ca0523bb47c73d3e39b52f3cf6b6a85a0578
khasan0330 <93327758+khasan0330@users.noreply.github.com>
2023-01-10 16:04:16 +0500

    celery по расписанию

--- django_2/conf/celery.py
+++ django_2/conf/celery.py
@@ -1,5 +1,6 @@
 import os
 from celery import Celery
+from celery.schedules import crontab
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'conf.settings')
@@ -7,3 +8,13 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'conf.settings')
 app = Celery('conf')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+
+# Запуск задач по расписанию
+app.conf.beat_schedule = {
+    'send-email-every-5-minute': {
+        'task': 'shop.tasks.send_spam',
+        'schedule': crontab(minute='*/5'),
+
+    },
+
+}
--- django_2/shop/tasks.py
+++ django_2/shop/tasks.py
@@ -1,7 +1,17 @@
 from conf.celery import app
 from .service import send
+from .models import Mail  # Для Запуска задач по расписанию
 
 
 @app.task
 def silent_send(user_email):
+    """Выполнение CPU-bound задач в фоне в Django"""
     send(user_email)
+
+
+@app.task
+def send_spam():
+    """Запуск задач по расписанию"""
+    for user_email in Mail.objects.all():
+        send(user_email)
+
